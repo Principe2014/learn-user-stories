e11a7cdfe78b9a549ae164052195ffdfb2d5c904
Principe2014 <112664140+Principe2014@users.noreply.github.com>
2024-09-16 19:11:08 -0400

    Create bank-demo.ts

---
+++ bank-demo/src/bank-demo.ts
@@ -0,0 +1,47 @@
+
+interface BankAccount {
+    name: string;
+    age: number;
+    accountNumber: string;
+    balance: number;
+}
+
+/**
+ *  Bank class that manages all bank accounts in the bank
+ */
+
+export default class Bank {
+    
+    private accounts: BankAccount[] = [];
+
+    /**
+     * Method to find a bank account in the bank
+     * @param {string} accountNumber Account number of the bank account to find
+     * @returns Bank account if found, undefined otherwise
+     */
+    private findAccount(accountNumber: string): BankAccount | undefined {
+        return this.accounts.find(account => account.accountNumber === accountNumber);
+    }
+
+    /**
+     * 
+     * @param name -- name of the customer
+     * @param age -- age of the customer
+     * @param accountNumber -- account number of the customer
+     * @returns BankAccount -- the created account
+     */
+    public createAccount(name: string, age: number, accountNumber: string): BankAccount {
+        const isAccExists = this.findAccount(accountNumber);
+        if(isAccExists) {
+            throw new Error("Account already exists");
+        }
+        const account: BankAccount = {
+            name,
+            age,
+            accountNumber,
+            balance: 0
+        };
+        this.accounts.push(account);
+        return account;
+    }
+}
